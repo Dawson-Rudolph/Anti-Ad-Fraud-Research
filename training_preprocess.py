ddc38a21f945273a493c0f11b3ce5a58adacb852
hermon1738 <117963478+hermon1738@users.noreply.github.com>
2024-10-09 20:48:27 -0500

    Preprocessed data and prepared environment setup
    - Using train_sample.csv to set up the environment as train.csv is too large.
    - Encapsulated data preprocessing in a function for easier integration with ML models.

--- training_preprocess.py
+++ training_preprocess.py
@@ -22,28 +22,31 @@ dtypes = {
     'is_attributed': 'uint8',
 }
 
-#read train file, sets datatypes, should parse dates in relevent columns correctly, convert to dataframe
-#here I'm using sample to save space, actual training set is 'train.csv'
-dataset = pd.read_csv('train_sample.csv', dtype=dtypes, parse_dates=['click_time']) 
+# wraped the data preprocessing to a function so it can be called in the ML models
+def preprocess_data():
+    #read train file, sets datatypes, should parse dates in relevent columns correctly, convert to dataframe
+    #here I'm using sample to save space, actual training set is 'train.csv'
+    dataset = pd.read_csv('train_sample.csv', dtype=dtypes, parse_dates=['click_time']) 
 
-#converts to date_time objects
-dataset['click_time'] = pd.to_datetime(dataset['click_time'])
+    #converts to date_time objects
+    dataset['click_time'] = pd.to_datetime(dataset['click_time'])
 
-#add columns for day, hour, minute, second
-dataset['day'], dataset['hour'], dataset['minute'], dataset['second'] = [
+    #add columns for day, hour, minute, second
+    dataset['day'], dataset['hour'], dataset['minute'], dataset['second'] = [
 
-    dataset['click_time'].dt.day.astype('uint8'),
-    dataset['click_time'].dt.hour.astype('uint8'),
-    dataset['click_time'].dt.minute.astype('uint8'),
-    dataset['click_time'].dt.second.astype('uint8')
+        dataset['click_time'].dt.day.astype('uint8'),
+        dataset['click_time'].dt.hour.astype('uint8'),
+        dataset['click_time'].dt.minute.astype('uint8'),
+        dataset['click_time'].dt.second.astype('uint8')
 
-]
+    ]
 
-#removes click_time and attributed_time from the dataframe
-#click_time is no longer necessary due to separation and creation of day, hour, minute, second
-#attributed_time is not necessary due to conveying the same information mathematically as is_attributed
-dataset.drop(columns=['click_time', 'attributed_time'])
-
+    #removes click_time and attributed_time from the dataframe
+    #click_time is no longer necessary due to separation and creation of day, hour, minute, second
+    #attributed_time is not necessary due to conveying the same information mathematically as is_attributed
+    # Drop the specified columns in-place to save memory, ignore errors if columns don't exist
+    dataset.drop(columns=['click_time', 'attributed_time'], inplace=True, errors='ignore')
+    return dataset
 
 #below is simply code to make a pie chart and a line to make a corr matrix that I haven't had
 #time to mess around with yet.

cf89f3ab125dad32d329164b4ec58816ae40424b
owHeusch <heuscello+git@gmail.com>
2024-11-06 18:45:31 -0600

    updated comment from review

--- training_preprocess.py
+++ training_preprocess.py
@@ -12,38 +12,42 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#define datatypes for non-datetime data
-dtypes = {
-    'ip': 'uint32',
-    'app': 'uint16',
-    'device': 'uint16',
-    'os': 'uint32',
-    'channel': 'uint32',
-    'is_attributed': 'uint8',
-}
+#csv file features
+#ip, app, channel, device, os, click_time, attributed_time, is_attributed
 
-#read train file, sets datatypes, should parse dates in relevent columns correctly, convert to dataframe
-#here I'm using sample to save space, actual training set is 'new_train.csv'
-dataset = pd.read_csv('train_sample.csv', dtype=dtypes, parse_dates=['click_time']) 
+def pre_process_train(filename : str):
+    #define datatypes for non-datetime data
+    dtypes = {
+        'ip': 'uint32',
+        'app': 'uint16',
+        'device': 'uint16',
+        'os': 'uint32',
+        'channel': 'uint32',
+        'is_attributed': 'uint8',
+    }
 
-#converts to date_time objects
-dataset['click_time'] = pd.to_datetime(dataset['click_time'])
+    #read train file, sets datatypes, should parse dates in relevent columns correctly, convert to dataframe
+    dataset = pd.read_csv(filename, dtype=dtypes, parse_dates=['click_time']) 
 
-#add columns for day, hour, minute, second
-dataset['day'], dataset['hour'], dataset['minute'], dataset['second'] = [
+    #converts to date_time objects
+    dataset['click_time'] = pd.to_datetime(dataset['click_time'])
 
-    dataset['click_time'].dt.day.astype('uint8'),
-    dataset['click_time'].dt.hour.astype('uint8'),
-    dataset['click_time'].dt.minute.astype('uint8'),
-    dataset['click_time'].dt.second.astype('uint8')
+    #add columns for day, hour, minute, second
+    dataset['day'], dataset['hour'], dataset['minute'], dataset['second'] = [
 
-]
+        dataset['click_time'].dt.day.astype('uint8'),
+        dataset['click_time'].dt.hour.astype('uint8'),
+        dataset['click_time'].dt.minute.astype('uint8'),
+        dataset['click_time'].dt.second.astype('uint8')
 
-#removes click_time and attributed_time from the dataframe
-#click_time is no longer necessary due to separation and creation of day, hour, minute, second
-#attributed_time is not necessary due to conveying the same information mathematically as is_attributed
-dataset = dataset.drop(columns=['click_time', 'attributed_time'])
+    ]
 
+    #removes click_time and attributed_time from the dataframe
+    #click_time is no longer necessary due to separation and creation of day, hour, minute, second
+    #attributed_time is not necessary due to conveying the same information mathematically as is_attributed
+    dataset.drop(columns=['click_time', 'attributed_time'], inplace=True)
+
+    return dataset
 
 #below is simply code to make a pie chart and a line to make a corr matrix that I haven't had
 #time to mess around with yet.
